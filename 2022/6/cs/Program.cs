5acb34f609477cc47ff9784a96749b4209fb65da
Chris Ayers <clayers@gmail.com>
2022-12-06 08:48:15 -0500

    Day 6

---
+++ 2022/6/cs/Program.cs
@@ -0,0 +1,27 @@
+﻿//https://adventofcode.com/2022/day/6
+var input = await File.ReadAllLinesAsync("../input.txt");
+
+var buffer = new Queue<char>(4);
+int i = 0;
+
+var marker = input
+    .Select(x =>
+    {
+        i = 0;
+        buffer.Clear();
+        //part1
+        // while (i < x.Length && buffer.Count < 4)
+        //part2
+        while (i < x.Length && buffer.Count < 14)
+        {
+            while(buffer.Contains(x[i]))
+            {
+                buffer.Dequeue();
+            }
+            buffer.Enqueue(x[i++]);
+        }
+        return i;
+    })
+    .ToList();
+
+marker.ForEach(x => Console.Write(x));
