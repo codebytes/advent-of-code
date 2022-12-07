5b3c51f00b6cc597f238e39df8b0d1cd2d1020e5
Chris Ayers <clayers@gmail.com>
2022-12-07 13:30:26 -0500

    tried a few older events

---
+++ 2015/1/cs/Program.cs
@@ -0,0 +1,25 @@
+﻿//https://adventofcode.com/2015/day/1
+var input = await File.ReadAllLinesAsync("../input.txt");
+var directions = input.First();
+
+int floor = 0;
+bool inBasement = false;
+for (int i = 0; i < directions.Length; i++)
+{
+    floor += directions[i] switch
+    {
+        '(' => 1,
+        ')' => -1,
+        _ => 0
+    };
+    if(floor < 0)
+    {
+        if(!inBasement)
+        {
+            Console.WriteLine($"First time in basement at step: {i+1}");
+            inBasement = true;
+        }
+    }
+}
+
+Console.WriteLine(floor);
---
+++ 2015/2/cs/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.RegularExpressions;
+//https://adventofcode.com/2015/day/2
+var input = await File.ReadAllLinesAsync("../input.txt");
+
+Regex dimensions = new Regex(@"(?<length>\d+)x(?<width>\d+)x(?<height>\d+)", RegexOptions.Compiled);
+
+var paperNeeds = input
+	.Select(x=>dimensions.Match(x))
+	.Select(x => new { l = int.Parse(x.Groups["length"].Value), w = int.Parse(x.Groups["width"].Value), h = int.Parse(x.Groups["height"].Value) })
+	.Select(x => new { area = Area(x.l, x.w, x.h), ribbon = Ribbon(x.l, x.w, x.h), smallest = Math.Min(Math.Min(x.l * x.w, x.w * x.h), x.h * x.l) })
+	.Select(x => new { amount = x.area + x.smallest, area = x.area, ribbon = x.ribbon});
+
+Console.WriteLine($"Paper Needs: {paperNeeds.Sum(x=>x.amount)}");
+Console.WriteLine($"Ribbon Needs: {paperNeeds.Sum(x=>x.ribbon)}");
+
+int Area(int l, int w, int h)
+{
+	return 2*l*w + 2*w*h + 2*h*l;
+}
+
+int Ribbon(int l, int w, int h)
+{
+	var sides = new int[] { l, w, h };
+	var smallerSides = sides.Order().Take(2);
+	return smallerSides.Sum() * 2 + l * w * h;
+}
