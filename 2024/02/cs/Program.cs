5a28888a4d942b0e19a461d1967043406c7ba6e3
Chris Ayers <clayers@gmail.com>
2024-12-03 00:11:13 -0500

    add solution for 2024 day 3 with C# implementation and sample input

--- 2024/02/cs/Program.cs
+++ 2024/02/cs/Program.cs
@@ -2,31 +2,29 @@
 // var input = await File.ReadAllLinesAsync("../sample.txt");
 var input = await File.ReadAllLinesAsync("../input.txt");
 
-var reports = input.Select(x => 
+var reports = input.Select(x =>
                     x.Split(' ')
                     .Select(int.Parse)
                     .ToArray())
                 .ToArray();
-
-int count=0;
 
 bool IsSafe(int[] levels)
 {
     var increasing = levels[1] > levels[0];
     return levels.Skip(1)
                  .Select((level, i) => level - levels[i])
-                 .All(diff => 
-                    Math.Abs(diff) >= 1 && 
-                    Math.Abs(diff) <= 3 && 
-                    (increasing 
-                        ? diff >= 0 
+                 .All(diff =>
+                    Math.Abs(diff) >= 1 &&
+                    Math.Abs(diff) <= 3 &&
+                    (increasing
+                        ? diff >= 0
                         : diff <= 0));
 }
 
 var safeReportsPart1 = reports.Count(IsSafe);
 Console.WriteLine($"Part 1: {safeReportsPart1}");
 
-var safeReportsPart2 = reports.Count(levels => 
+var safeReportsPart2 = reports.Count(levels =>
     Enumerable.Range(0, levels.Length)
               .Select(i => levels.Where((_, index) => index != i).ToArray())
               .Any(modifiedLevels => IsSafe(modifiedLevels))
---
+++ 2024/03/cs/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Text.RegularExpressions;
+
+//https://adventofcode.com/2024/day/2
+// var input = await File.ReadAllTextAsync("../sample.txt");
+var input = await File.ReadAllTextAsync("../input.txt");
+
+int SumOfMultiplicationsPart1(string input)
+{
+    var matches = Regex.Matches(input, @"mul\((\d+),(\d+)\)");
+    int sum = 0;
+    foreach (Match match in matches)
+    {
+        int x = int.Parse(match.Groups[1].Value);
+        int y = int.Parse(match.Groups[2].Value);
+        sum += x * y;
+    }
+    return sum;
+}
+
+int SumOfMultiplicationsPart2(string input)
+{
+    var matches = Regex.Matches(input, @"(mul\((\d+),(\d+)\))|(do\(\))|(don't\(\))");
+    int sum = 0;
+    bool isEnabled = true;
+    foreach (Match match in matches)
+    {
+        switch (match.Value)
+        {
+            case "do()":
+                isEnabled = true;
+                break;
+            case "don't()":
+                isEnabled = false;
+                break;
+            case { } s when isEnabled && s.StartsWith("mul("):
+                int x = int.Parse(match.Groups[2].Value);
+                int y = int.Parse(match.Groups[3].Value);
+                sum += x * y;
+                break;
+        }
+    }
+    return sum;
+}
+
+Console.WriteLine("Part 1: " + SumOfMultiplicationsPart1(input));
+Console.WriteLine("Part 2: " + SumOfMultiplicationsPart2(input));
+
